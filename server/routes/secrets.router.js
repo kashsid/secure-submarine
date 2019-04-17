317e8634e04bf392a897f80f71ef2dff47db8629
kashsid <kashifsiddique@gmail.com>
2019-04-17 15:14:41 -0500

    implements query to show secerets based on logged in  user

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated,(req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    //pool.query('SELECT * FROM "secret";')
+    const action = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`
+    pool.query(action, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
 });
+
 
 module.exports = router;
